6098fec96f510de063b3a8cc15cba76874c389db
Kræn Hansen <kraen.hansen@mongodb.com>
2025-05-05 16:54:59 +0200

    Drive autolink from Gradle

--- packages/react-native-node-api-modules/src/node/cli/android.ts
+++ packages/react-native-node-api-modules/src/node/cli/android.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert/strict";
 import fs from "node:fs";
 import path from "node:path";
 
@@ -7,6 +8,13 @@ import {
   LinkModuleResult,
   type LinkModuleOptions,
 } from "./link-modules";
+
+const ANDROID_ARCHITECTURES = [
+  "arm64-v8a",
+  "armeabi-v7a",
+  "x86_64",
+  "x86",
+] as const;
 
 export async function linkAndroidDir({
   incremental,
@@ -32,6 +40,24 @@ export async function linkAndroidDir({
 
   await fs.promises.rm(outputPath, { recursive: true, force: true });
   await fs.promises.cp(modulePath, outputPath, { recursive: true });
+  for (const arch of ANDROID_ARCHITECTURES) {
+    const archPath = path.join(outputPath, arch);
+    if (!fs.existsSync(archPath)) {
+      // Skip missing architectures
+      continue;
+    }
+    const libraryDirents = await fs.promises.readdir(archPath, {
+      withFileTypes: true,
+    });
+    assert(libraryDirents.length === 1, "Expected exactly one library file");
+    const [libraryDirent] = libraryDirents;
+    assert(libraryDirent.isFile(), "Expected a library file");
+    const libraryPath = path.join(libraryDirent.parentPath, libraryDirent.name);
+    await fs.promises.rename(
+      libraryPath,
+      path.join(archPath, `lib${libraryName}.so`)
+    );
+  }
   await fs.promises.rm(path.join(outputPath, MAGIC_FILENAME), {
     recursive: true,
   });
