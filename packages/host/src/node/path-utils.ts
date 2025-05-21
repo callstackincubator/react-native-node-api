bb8b7b3a573dccb1880a62c722cb6cd626fbc43b
Mariusz Pasinski <mariusz.pasinski@callstack.com>
2025-05-21 14:32:43 +0200

    feat: relax the condition for CJS modules without file exts
    This small change relaxes the condition for taking the shortcut, as CommonJS modules (in contrast to ESM) do not require developers to explicitly include the file extensions. The Node.js module resolution algorithm (https://nodejs.org/api/modules.html#all-together) in step 4 of LOAD_AS_FILE(X) would try appending the `.node` extension.
    
    In theory, we should make sure that other extensions checked in previous steps are not present, but given that we are implementing it for `requireNodeAddon()`, it should be safe to skip those.
    
    # Conflicts:
    #	packages/host/src/node/path-utils.ts

--- packages/host/src/node/path-utils.ts
+++ packages/host/src/node/path-utils.ts
@@ -31,6 +31,13 @@ const packageNameCache = new Map<string, string>();
  * TODO: Consider checking for a specific platform extension.
  */
 export function isNodeApiModule(modulePath: string): boolean {
+  {
+    // HACK: Take a shortcut (if applicable): existing `.node` files are addons
+    try {
+      fs.accessSync(modulePath.endsWith(".node") ? modulePath : `${modulePath}.node`);
+      return true;
+    } catch {}
+  }
   const dir = path.dirname(modulePath);
   const baseName = path.basename(modulePath, ".node");
   let entries: string[];
