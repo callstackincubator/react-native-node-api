d6d57a18e2928119d8e3219db7476a83dc4c1da4
Kamil Paradowski <kamil.paradowski@callstack.com>
2025-07-11 13:48:33 +0200

    Removed gc related tests

--- packages/node-addon-examples/tests/buffers/addon.js
+++ packages/node-addon-examples/tests/buffers/addon.js
@@ -10,22 +10,10 @@ const toLocaleString = (text) => {
     .join("");
 };
 
-console.log(toLocaleString(addon.newExternalBuffer()), addon.theText);
 console.log(addon.newBuffer().toLocaleString(), addon.theText);
-console.log("gc1");
-console.log(addon.getDeleterCallCount(), 0);
-global.gc();
-console.log(addon.getDeleterCallCount(), 1);
-console.log(addon.getDeleterCallCount(), 1);
+console.log(toLocaleString(addon.newExternalBuffer()), addon.theText);
 console.log(addon.copyBuffer(), addon.theText);
-
 let buffer = addon.staticBuffer();
 console.log(addon.bufferHasInstance(buffer), true);
 console.log(addon.bufferInfo(buffer), true);
-buffer = null;
-global.gc();
-console.log(addon.getDeleterCallCount(), 1);
-console.log("gc2");
-console.log(addon.getDeleterCallCount(), 2);
-
 addon.invalidObjectAsBuffer({});
