6cf32e900a3e65e92dc2d5eaa798256062545f1c
Kamil Paradowski <kamil.paradowski@callstack.com>
2025-07-16 11:47:36 +0200

    Added Android OnLoad

--- packages/host/android/src/main/cpp/OnLoad.cpp
+++ packages/host/android/src/main/cpp/OnLoad.cpp
@@ -3,15 +3,17 @@
 #include <ReactCommon/CxxTurboModuleUtils.h>
 
 #include <CxxNodeApiHostModule.hpp>
+#include <RuntimeNodeApiAsync.hpp>
 #include <WeakNodeApiInjector.hpp>
 
 // Called when the library is loaded
-jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   callstack::nodeapihost::injectIntoWeakNodeApi();
   // Register the C++ TurboModule
   facebook::react::registerCxxModuleToGlobalModuleMap(
       callstack::nodeapihost::CxxNodeApiHostModule::kModuleName,
       [](std::shared_ptr<facebook::react::CallInvoker> jsInvoker) {
+        callstack::nodeapihost::setCallInvoker(jsInvoker);
         return std::make_shared<callstack::nodeapihost::CxxNodeApiHostModule>(
             jsInvoker);
       });
