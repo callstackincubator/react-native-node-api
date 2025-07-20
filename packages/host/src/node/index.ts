3c0ffc8cfd53dd70a5d972b41765c71379e82c3c
Kræn Hansen <kraen.hansen@mongodb.com>
2025-07-20 11:13:30 +0200

    Add isSupportedTriplet to host

--- packages/host/src/node/index.ts
+++ packages/host/src/node/index.ts
@@ -1,10 +1,11 @@
 export {
+  SUPPORTED_TRIPLETS,
   ANDROID_TRIPLETS,
   APPLE_TRIPLETS,
-  SUPPORTED_TRIPLETS,
+  type SupportedTriplet,
   type AndroidTriplet,
   type AppleTriplet,
-  type SupportedTriplet,
+  isSupportedTriplet,
   isAppleTriplet,
   isAndroidTriplet,
 } from "./prebuilds/triplets.js";
