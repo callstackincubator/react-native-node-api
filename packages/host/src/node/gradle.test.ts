cf40320a37cda31117e3010a1ffe707e101672bd
Kræn Hansen <mail@kraenhansen.dk>
2025-09-15 14:55:45 +0200

    Skip Gradle tests by default and enable it on CI

--- packages/host/src/node/gradle.test.ts
+++ packages/host/src/node/gradle.test.ts
@@ -7,55 +7,60 @@ const PACKAGE_ROOT = path.join(__dirname, "../..");
 const MONOREPO_ROOT = path.join(PACKAGE_ROOT, "../..");
 const TEST_APP_ANDROID_PATH = path.join(MONOREPO_ROOT, "apps/test-app/android");
 
-describe("Gradle tasks", () => {
-  describe("checkHermesOverride task", () => {
-    it("should fail if REACT_NATIVE_OVERRIDE_HERMES_DIR is not set", () => {
-      const { status, stdout, stderr } = cp.spawnSync(
-        "sh",
-        ["gradlew", "react-native-node-api:checkHermesOverride"],
-        {
-          cwd: TEST_APP_ANDROID_PATH,
-          env: {
-            ...process.env,
-            REACT_NATIVE_OVERRIDE_HERMES_DIR: undefined,
+describe(
+  "Gradle tasks",
+  // Skipping these tests by default, as they download a lot and takes a long time
+  { skip: process.env.ENABLE_GRADLE_TESTS !== "true" },
+  () => {
+    describe("checkHermesOverride task", () => {
+      it("should fail if REACT_NATIVE_OVERRIDE_HERMES_DIR is not set", () => {
+        const { status, stdout, stderr } = cp.spawnSync(
+          "sh",
+          ["gradlew", "react-native-node-api:checkHermesOverride"],
+          {
+            cwd: TEST_APP_ANDROID_PATH,
+            env: {
+              ...process.env,
+              REACT_NATIVE_OVERRIDE_HERMES_DIR: undefined,
+            },
+            encoding: "utf-8",
           },
-          encoding: "utf-8",
-        },
-      );
+        );
 
-      assert.notEqual(status, 0, `Expected failure: ${stdout} ${stderr}`);
-      assert.match(
-        stderr,
-        /React Native Node-API needs a custom version of Hermes with Node-API enabled/,
-      );
-      assert.match(
-        stderr,
-        /Run the following in your terminal, to clone Hermes and instruct React Native to use it/,
-      );
-      assert.match(
-        stderr,
-        /export REACT_NATIVE_OVERRIDE_HERMES_DIR=`npx react-native-node-api vendor-hermes --silent --force`/,
-      );
-      assert.match(
-        stderr,
-        /And follow this guide to build React Native from source/,
-      );
+        assert.notEqual(status, 0, `Expected failure: ${stdout} ${stderr}`);
+        assert.match(
+          stderr,
+          /React Native Node-API needs a custom version of Hermes with Node-API enabled/,
+        );
+        assert.match(
+          stderr,
+          /Run the following in your terminal, to clone Hermes and instruct React Native to use it/,
+        );
+        assert.match(
+          stderr,
+          /export REACT_NATIVE_OVERRIDE_HERMES_DIR=`npx react-native-node-api vendor-hermes --silent --force`/,
+        );
+        assert.match(
+          stderr,
+          /And follow this guide to build React Native from source/,
+        );
+      });
     });
-  });
 
-  describe("linkNodeApiModules task", () => {
-    it("should call the CLI to autolink", () => {
-      const { status, stdout, stderr } = cp.spawnSync(
-        "sh",
-        ["gradlew", "react-native-node-api:linkNodeApiModules"],
-        {
-          cwd: TEST_APP_ANDROID_PATH,
-          encoding: "utf-8",
-        },
-      );
+    describe("linkNodeApiModules task", () => {
+      it("should call the CLI to autolink", () => {
+        const { status, stdout, stderr } = cp.spawnSync(
+          "sh",
+          ["gradlew", "react-native-node-api:linkNodeApiModules"],
+          {
+            cwd: TEST_APP_ANDROID_PATH,
+            encoding: "utf-8",
+          },
+        );
 
-      assert.equal(status, 0, `Expected failure: ${stdout} ${stderr}`);
-      assert.match(stdout, /Auto-linking Node-API modules/);
+        assert.equal(status, 0, `Expected failure: ${stdout} ${stderr}`);
+        assert.match(stdout, /Auto-linking Node-API modules/);
+      });
     });
-  });
-});
+  },
+);
