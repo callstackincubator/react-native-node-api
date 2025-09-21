b0f6f61b75bb6b0188d946c18e33d61b9b77b300
Kræn Hansen <mail@kraenhansen.dk>
2025-09-21 11:56:31 +0200

    Fix tests

--- packages/host/src/node/gradle.test.ts
+++ packages/host/src/node/gradle.test.ts
@@ -12,11 +12,11 @@ describe(
   // Skipping these tests by default, as they download a lot and takes a long time
   { skip: process.env.ENABLE_GRADLE_TESTS !== "true" },
   () => {
-    describe("checkHermesOverride task", () => {
+    describe("linkNodeApiModules task", () => {
       it("should fail if REACT_NATIVE_OVERRIDE_HERMES_DIR is not set", () => {
         const { status, stdout, stderr } = cp.spawnSync(
           "sh",
-          ["gradlew", "react-native-node-api:checkHermesOverride"],
+          ["gradlew", "react-native-node-api:linkNodeApiModules"],
           {
             cwd: TEST_APP_ANDROID_PATH,
             env: {
@@ -45,15 +45,18 @@ describe(
           /And follow this guide to build React Native from source/,
         );
       });
-    });
 
-    describe("linkNodeApiModules task", () => {
       it("should call the CLI to autolink", () => {
         const { status, stdout, stderr } = cp.spawnSync(
           "sh",
           ["gradlew", "react-native-node-api:linkNodeApiModules"],
           {
             cwd: TEST_APP_ANDROID_PATH,
+            env: {
+              ...process.env,
+              // We're passing some directory which exists
+              REACT_NATIVE_OVERRIDE_HERMES_DIR: __dirname,
+            },
             encoding: "utf-8",
           },
         );
