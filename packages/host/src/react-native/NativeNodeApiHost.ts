9f1a30112ed0d12a32985f288fb3485fa55e4b07
Kræn Hansen <mail@kraenhansen.dk>
2025-10-18 09:49:53 +0200

    Fix requireNodeAddon return type (#267)

--- packages/host/src/react-native/NativeNodeApiHost.ts
+++
@@ -1,8 +0,0 @@
-import type { TurboModule } from "react-native";
-import { TurboModuleRegistry } from "react-native";
-
-export interface Spec extends TurboModule {
-  requireNodeAddon(libraryName: string): void;
-}
-
-export default TurboModuleRegistry.getEnforcing<Spec>("NodeApiHost");
--- packages/host/src/react-native/index.ts
+++ packages/host/src/react-native/index.ts
@@ -1,3 +1,14 @@
-import native from "./NativeNodeApiHost";
+import { type TurboModule, TurboModuleRegistry } from "react-native";
 
-export const requireNodeAddon = native.requireNodeAddon.bind(native);
+export interface Spec extends TurboModule {
+  requireNodeAddon<T = unknown>(libraryName: string): T;
+}
+
+const native = TurboModuleRegistry.getEnforcing<Spec>("NodeApiHost");
+
+/**
+ * Loads a native Node-API addon by filename.
+ */
+export function requireNodeAddon<T = unknown>(libraryName: string): T {
+  return native.requireNodeAddon<T>(libraryName);
+}
