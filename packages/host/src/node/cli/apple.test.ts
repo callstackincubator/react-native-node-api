87a20f87a45f232bb759266f268fb8a70ca65f90
Kræn Hansen <mail@kraenhansen.dk>
2025-10-21 23:05:25 +0200

    Support linking of both flat and versioned frameworks

--- packages/host/src/node/cli/apple.test.ts
+++ packages/host/src/node/cli/apple.test.ts
@@ -2,54 +2,14 @@ import assert from "node:assert/strict";
 import { describe, it } from "node:test";
 import path from "node:path";
 import fs from "node:fs";
+import cp from "node:child_process";
 
-import {
-  determineInfoPlistPath,
-  readInfoPlist,
-  updateInfoPlist,
-} from "./apple";
+import { linkFlatFramework, readAndParsePlist } from "./apple";
 import { setupTempDirectory } from "../test-utils";
 
-describe("apple", () => {
-  describe("determineInfoPlistPath", () => {
-    it("should find Info.plist files in unversioned frameworks", (context) => {
-      const infoPlistContents = `<?xml version="1.0" encoding="UTF-8"?>...`;
-      const infoPlistSubPath = "Info.plist";
-      const tempDirectoryPath = setupTempDirectory(context, {
-        [infoPlistSubPath]: infoPlistContents,
-      });
-
-      assert.strictEqual(
-        determineInfoPlistPath(tempDirectoryPath),
-        path.join(tempDirectoryPath, infoPlistSubPath),
-      );
-    });
-
-    it("should find Info.plist files in versioned frameworks", (context) => {
-      const infoPlistContents = `<?xml version="1.0" encoding="UTF-8"?>...`;
-      const infoPlistSubPath = "Versions/Current/Resources/Info.plist";
-      const tempDirectoryPath = setupTempDirectory(context, {
-        [infoPlistSubPath]: infoPlistContents,
-      });
-
-      assert.strictEqual(
-        determineInfoPlistPath(tempDirectoryPath),
-        path.join(tempDirectoryPath, infoPlistSubPath),
-      );
-    });
-
-    it("should throw if Info.plist is missing from framework", (context) => {
-      const tempDirectoryPath = setupTempDirectory(context, {});
-
-      assert.throws(
-        () => determineInfoPlistPath(tempDirectoryPath),
-        /Unable to locate an Info.plist file within framework./,
-      );
-    });
-  });
-
+describe("apple", { skip: process.platform !== "darwin" }, () => {
   describe("readInfoPlist", () => {
-    it("should read Info.plist contents", async (context) => {
+    it("should read Info.plist contents, plus extra keys not in schema", async (context) => {
       const infoPlistContents = `
         <?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -57,8 +17,10 @@ describe("apple", () => {
           <dict>
             <key>CFBundleExecutable</key>
             <string>ExecutableFileName</string>
-            <key>CFBundleIconFile</key>
-            <string>AppIcon</string>
+            <key>CFBundlePackageType</key>
+            <string>FMWK</string>
+            <key>CFBundleInfoDictionaryVersion</key>
+            <string>6.0</string>
           </dict>
         </plist>
       `;
@@ -68,10 +30,11 @@ describe("apple", () => {
       });
       const infoPlistPath = path.join(tempDirectoryPath, infoPlistSubPath);
 
-      const contents = await readInfoPlist(infoPlistPath);
+      const contents = await readAndParsePlist(infoPlistPath);
       assert.deepEqual(contents, {
         CFBundleExecutable: "ExecutableFileName",
-        CFBundleIconFile: "AppIcon",
+        CFBundlePackageType: "FMWK",
+        CFBundleInfoDictionaryVersion: "6.0",
       });
     });
 
@@ -80,104 +43,149 @@ describe("apple", () => {
       const infoPlistPath = path.join(tempDirectoryPath, "Info.plist");
 
       await assert.rejects(
-        () => readInfoPlist(infoPlistPath),
-        /Unable to read Info.plist at path/,
+        () => readAndParsePlist(infoPlistPath),
+        /Expected an Info.plist/,
       );
     });
   });
 
-  describe("updateInfoPlist", () => {
-    it(
-      "updates an xml plist",
-      { skip: process.platform !== "darwin" },
-      async (context) => {
-        const infoPlistSubPath = "Info.plist";
-        const tempDirectoryPath = setupTempDirectory(context, {
+  describe("linkFlatFramework", () => {
+    it("updates an xml plist, preserving extra keys", async (context) => {
+      const infoPlistSubPath = "Info.plist";
+      const tempDirectoryPath = setupTempDirectory(context, {
+        "foo.framework": {
           [infoPlistSubPath]: `
-          <?xml version="1.0" encoding="UTF-8"?>
-          <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-          <plist version="1.0">
-            <dict>
-              <key>CFBundleExecutable</key>
-              <string>addon</string>
-            </dict>
-          </plist>
-        `,
-        });
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+            <plist version="1.0">
+              <dict>
+                <key>CFBundleExecutable</key>
+                <string>addon</string>
+                <key>CFBundlePackageType</key>
+                <string>FMWK</string>
+                <key>CFBundleInfoDictionaryVersion</key>
+                <string>6.0</string>
+                <key>MyExtraKey</key>
+                <string>MyExtraValue</string>
+              </dict>
+            </plist>
+          `,
+        },
+      });
 
-        await updateInfoPlist({
-          frameworkPath: tempDirectoryPath,
-          oldLibraryName: "addon",
+      // Create a dummy binary file
+      cp.spawnSync("clang", [
+        "-dynamiclib",
+        "-o",
+        path.join(tempDirectoryPath, "foo.framework", "addon"),
+        "-xc",
+        "/dev/null",
+      ]);
+
+      await linkFlatFramework({
+        frameworkPath: path.join(tempDirectoryPath, "foo.framework"),
+        newLibraryName: "new-addon-name",
+      });
+
+      const contents = await fs.promises.readFile(
+        path.join(
+          tempDirectoryPath,
+          "new-addon-name.framework",
+          infoPlistSubPath,
+        ),
+        "utf-8",
+      );
+      assert.match(contents, /<\?xml version="1.0" encoding="UTF-8"\?>/);
+      assert.match(
+        contents,
+        /<key>CFBundleExecutable<\/key>\s*<string>new-addon-name<\/string>/,
+      );
+
+      // Assert the install name was updated correctly
+      const { stdout: otoolOutput } = cp.spawnSync(
+        "otool",
+        [
+          "-L",
+          path.join(
+            tempDirectoryPath,
+            "new-addon-name.framework",
+            "new-addon-name",
+          ),
+        ],
+        { encoding: "utf-8" },
+      );
+      assert.match(
+        otoolOutput,
+        /@rpath\/new-addon-name.framework\/new-addon-name/,
+      );
+
+      // It should preserve extra keys
+      assert.match(
+        contents,
+        /<key>MyExtraKey<\/key>\s*<string>MyExtraValue<\/string>/,
+      );
+    });
+
+    it("converts a binary plist to xml", async (context) => {
+      const tempDirectoryPath = setupTempDirectory(context, {});
+      await fs.promises.mkdir(path.join(tempDirectoryPath, "foo.framework"));
+      // Write a binary plist file
+      const binaryPlistContents = Buffer.from(
+        // Generated running "base64 -i <path-to-binary-plist>" on a plist file from a framework in the node-examples package
+        "YnBsaXN0MDDfEBUBAgMEBQYHCAkKCwwNDg8QERITFBUWFxgZGhscHR4cICEiIyQiJSYnJChfEBNCdWlsZE1hY2hpbmVPU0J1aWxkXxAZQ0ZCdW5kbGVEZXZlbG9wbWVudFJlZ2lvbl8QEkNGQnVuZGxlRXhlY3V0YWJsZV8QEkNGQnVuZGxlSWRlbnRpZmllcl8QHUNGQnVuZGxlSW5mb0RpY3Rpb25hcnlWZXJzaW9uXxATQ0ZCdW5kbGVQYWNrYWdlVHlwZV8QGkNGQnVuZGxlU2hvcnRWZXJzaW9uU3RyaW5nXxARQ0ZCdW5kbGVTaWduYXR1cmVfEBpDRkJ1bmRsZVN1cHBvcnRlZFBsYXRmb3Jtc18QD0NGQnVuZGxlVmVyc2lvbl8QFUNTUmVzb3VyY2VzRmlsZU1hcHBlZFpEVENvbXBpbGVyXxAPRFRQbGF0Zm9ybUJ1aWxkXkRUUGxhdGZvcm1OYW1lXxARRFRQbGF0Zm9ybVZlcnNpb25aRFRTREtCdWlsZFlEVFNES05hbWVXRFRYY29kZVxEVFhjb2RlQnVpbGRfEBBNaW5pbXVtT1NWZXJzaW9uXlVJRGV2aWNlRmFtaWx5VjI0RzIzMVdFbmdsaXNoVWFkZG9uXxAPZXhhbXBsZV82LmFkZG9uUzYuMFRGTVdLUzEuMFQ/Pz8/oR9fEA9pUGhvbmVTaW11bGF0b3IJXxAiY29tLmFwcGxlLmNvbXBpbGVycy5sbHZtLmNsYW5nLjFfMFYyMkMxNDZfEA9pcGhvbmVzaW11bGF0b3JUMTguMl8QE2lwaG9uZXNpbXVsYXRvcjE4LjJUMTYyMFgxNkM1MDMyYaEpEAEACAA1AEsAZwB8AJEAsQDHAOQA+AEVAScBPwFKAVwBawF/AYoBlAGcAakBvAHLAdIB2gHgAfIB9gH7Af8CBAIGAhgCGQI+AkUCVwJcAnICdwKAAoIAAAAAAAACAQAAAAAAAAAqAAAAAAAAAAAAAAAAAAAChA==",
+        "base64",
+      );
+      await fs.promises.writeFile(
+        path.join(tempDirectoryPath, "foo.framework", "Info.plist"),
+        binaryPlistContents,
+      );
+
+      // Create a dummy binary file
+      cp.spawnSync("clang", [
+        "-dynamiclib",
+        "-o",
+        path.join(tempDirectoryPath, "foo.framework", "addon"),
+        "-xc",
+        "/dev/null",
+      ]);
+
+      await linkFlatFramework({
+        frameworkPath: path.join(tempDirectoryPath, "foo.framework"),
+        newLibraryName: "new-addon-name",
+      });
+
+      const contents = await fs.promises.readFile(
+        path.join(tempDirectoryPath, "new-addon-name.framework", "Info.plist"),
+        "utf-8",
+      );
+      assert.match(contents, /<\?xml version="1.0" encoding="UTF-8"\?>/);
+      assert.match(
+        contents,
+        /<key>CFBundleExecutable<\/key>\s*<string>new-addon-name<\/string>/,
+      );
+    });
+  });
+});
+
+describe("apple on non-darwin", { skip: process.platform === "darwin" }, () => {
+  it("throws", async (context) => {
+    const tempDirectoryPath = setupTempDirectory(context, {
+      ["Info.plist"]: '<?xml version="1.0" encoding="UTF-8"?>',
+    });
+
+    await assert.rejects(
+      () =>
+        linkFlatFramework({
+          frameworkPath: path.join(tempDirectoryPath, "Info.plist"),
           newLibraryName: "new-addon-name",
-        });
-
-        const contents = await fs.promises.readFile(
-          path.join(tempDirectoryPath, infoPlistSubPath),
-          "utf-8",
-        );
-        assert.match(contents, /<\?xml version="1.0" encoding="UTF-8"\?>/);
+        }),
+      (err) => {
+        assert(err instanceof Error);
         assert.match(
-          contents,
-          /<key>CFBundleExecutable<\/key>\s*<string>new-addon-name<\/string>/,
+          err.message,
+          /Linking Apple addons are only supported on macOS/,
         );
-      },
-    );
-
-    it(
-      "converts a binary plist to xml",
-      { skip: process.platform !== "darwin" },
-      async (context) => {
-        const tempDirectoryPath = setupTempDirectory(context, {});
-        // Write a binary plist file
-        const binaryPlistContents = Buffer.from(
-          // Generated running "base64 -i <path-to-binary-plist>" on a plist file from a framework in the node-examples package
-          "YnBsaXN0MDDfEBUBAgMEBQYHCAkKCwwNDg8QERITFBUWFxgZGhscHR4cICEiIyQiJSYnJChfEBNCdWlsZE1hY2hpbmVPU0J1aWxkXxAZQ0ZCdW5kbGVEZXZlbG9wbWVudFJlZ2lvbl8QEkNGQnVuZGxlRXhlY3V0YWJsZV8QEkNGQnVuZGxlSWRlbnRpZmllcl8QHUNGQnVuZGxlSW5mb0RpY3Rpb25hcnlWZXJzaW9uXxATQ0ZCdW5kbGVQYWNrYWdlVHlwZV8QGkNGQnVuZGxlU2hvcnRWZXJzaW9uU3RyaW5nXxARQ0ZCdW5kbGVTaWduYXR1cmVfEBpDRkJ1bmRsZVN1cHBvcnRlZFBsYXRmb3Jtc18QD0NGQnVuZGxlVmVyc2lvbl8QFUNTUmVzb3VyY2VzRmlsZU1hcHBlZFpEVENvbXBpbGVyXxAPRFRQbGF0Zm9ybUJ1aWxkXkRUUGxhdGZvcm1OYW1lXxARRFRQbGF0Zm9ybVZlcnNpb25aRFRTREtCdWlsZFlEVFNES05hbWVXRFRYY29kZVxEVFhjb2RlQnVpbGRfEBBNaW5pbXVtT1NWZXJzaW9uXlVJRGV2aWNlRmFtaWx5VjI0RzIzMVdFbmdsaXNoVWFkZG9uXxAPZXhhbXBsZV82LmFkZG9uUzYuMFRGTVdLUzEuMFQ/Pz8/oR9fEA9pUGhvbmVTaW11bGF0b3IJXxAiY29tLmFwcGxlLmNvbXBpbGVycy5sbHZtLmNsYW5nLjFfMFYyMkMxNDZfEA9pcGhvbmVzaW11bGF0b3JUMTguMl8QE2lwaG9uZXNpbXVsYXRvcjE4LjJUMTYyMFgxNkM1MDMyYaEpEAEACAA1AEsAZwB8AJEAsQDHAOQA+AEVAScBPwFKAVwBawF/AYoBlAGcAakBvAHLAdIB2gHgAfIB9gH7Af8CBAIGAhgCGQI+AkUCVwJcAnICdwKAAoIAAAAAAAACAQAAAAAAAAAqAAAAAAAAAAAAAAAAAAAChA==",
-          "base64",
-        );
-        const binaryPlistPath = path.join(tempDirectoryPath, "Info.plist");
-        await fs.promises.writeFile(binaryPlistPath, binaryPlistContents);
-
-        await updateInfoPlist({
-          frameworkPath: tempDirectoryPath,
-          oldLibraryName: "addon",
-          newLibraryName: "new-addon-name",
-        });
-
-        const contents = await fs.promises.readFile(binaryPlistPath, "utf-8");
-        assert.match(contents, /<\?xml version="1.0" encoding="UTF-8"\?>/);
-        assert.match(
-          contents,
-          /<key>CFBundleExecutable<\/key>\s*<string>new-addon-name<\/string>/,
-        );
-      },
-    );
-
-    it(
-      "throws when not on darwin",
-      { skip: process.platform === "darwin" },
-      async (context) => {
-        const tempDirectoryPath = setupTempDirectory(context, {
-          ["Info.plist"]: '<?xml version="1.0" encoding="UTF-8"?>',
-        });
-
-        await assert.rejects(
-          () =>
-            updateInfoPlist({
-              frameworkPath: tempDirectoryPath,
-              oldLibraryName: "addon",
-              newLibraryName: "new-addon-name",
-            }),
-          (err) => {
-            assert(err instanceof Error);
-            assert.match(err.message, /Failed to convert Info.plist at path/);
-            assert(err.cause instanceof Error);
-            assert.match(
-              err.cause.message,
-              /Updating Info.plist files are not supported on this platform/,
-            );
-            return true;
-          },
-        );
+        return true;
       },
     );
   });
--- packages/host/src/node/cli/apple.ts
+++ packages/host/src/node/cli/apple.ts
@@ -17,31 +17,38 @@ import {
 /**
  * Reads and parses a plist file, converting it to XML format if needed.
  */
-async function readAndParsePlist(plistPath: string): Promise<unknown> {
+export async function readAndParsePlist(plistPath: string): Promise<unknown> {
+  assert(fs.existsSync(plistPath), `Expected an Info.plist: ${plistPath}`);
+  // Try reading the file to see if it is already in XML format
   try {
-    // Convert to XML format if needed
-    assert(
-      process.platform === "darwin",
-      "Updating Info.plist files are not supported on this platform",
-    );
-    // Try reading the file to see if it is already in XML format
     const contents = await fs.promises.readFile(plistPath, "utf-8");
     if (contents.startsWith("<?xml")) {
       return plist.parse(contents) as unknown;
-    } else {
+    }
+
+    try {
+      // Convert to XML format if needed
+      assert(
+        process.platform === "darwin",
+        "Updating Info.plist files are not supported on this platform",
+      );
       await spawn("plutil", ["-convert", "xml1", plistPath], {
         outputMode: "inherit",
       });
-      // Read it again
-      return plist.parse(
-        await fs.promises.readFile(plistPath, "utf-8"),
-      ) as unknown;
+    } catch (cause) {
+      throw new Error(`Failed to convert plist to XML: ${plistPath}`, {
+        cause,
+      });
     }
-  } catch (error) {
-    throw new Error(
-      `Failed to convert plist at path "${plistPath}" to XML format`,
-      { cause: error },
-    );
+
+    return plist.parse(
+      // Read it again now that it is in XML format
+      await fs.promises.readFile(plistPath, "utf-8"),
+    ) as unknown;
+  } catch (cause) {
+    throw new Error(`Failed to read and parse plist at path "${plistPath}"`, {
+      cause,
+    });
   }
 }
 
@@ -58,8 +65,7 @@ const XcframeworkInfoSchema = zod.looseObject({
   XCFrameworkFormatVersion: zod.literal("1.0"),
 });
 
-export async function readXcframeworkInfo(xcframeworkPath: string) {
-  const infoPlistPath = path.join(xcframeworkPath, "Info.plist");
+export async function readXcframeworkInfo(infoPlistPath: string) {
   const infoPlist = await readAndParsePlist(infoPlistPath);
   return XcframeworkInfoSchema.parse(infoPlist);
 }
@@ -79,107 +85,151 @@ const FrameworkInfoSchema = zod.looseObject({
   CFBundleExecutable: zod.string(),
 });
 
-export async function readFrameworkInfo(frameworkPath: string) {
-  const infoPlistPath = path.join(frameworkPath, "Info.plist");
+export async function readFrameworkInfo(infoPlistPath: string) {
   const infoPlist = await readAndParsePlist(infoPlistPath);
   return FrameworkInfoSchema.parse(infoPlist);
 }
 
 export async function writeFrameworkInfo(
-  frameworkPath: string,
+  infoPlistPath: string,
   info: zod.infer<typeof FrameworkInfoSchema>,
 ) {
-  const infoPlistPath = path.join(frameworkPath, "Info.plist");
   const infoPlistXml = plist.build(info);
   await fs.promises.writeFile(infoPlistPath, infoPlistXml, "utf-8");
 }
 
-export function determineInfoPlistPath(frameworkPath: string) {
-  const checkedPaths = new Array<string>();
-
-  // First, assume it is an "unversioned" framework that keeps its Info.plist in
-  // the root. This is the convention for iOS, tvOS, and friends.
-  let infoPlistPath = path.join(frameworkPath, "Info.plist");
-
-  if (fs.existsSync(infoPlistPath)) {
-    return infoPlistPath;
-  }
-  checkedPaths.push(infoPlistPath);
-
-  // Next, assume it is a "versioned" framework that keeps its Info.plist
-  // under a subdirectory. This is the convention for macOS.
-  infoPlistPath = path.join(
-    frameworkPath,
-    "Versions/Current/Resources/Info.plist",
-  );
-
-  if (fs.existsSync(infoPlistPath)) {
-    return infoPlistPath;
-  }
-  checkedPaths.push(infoPlistPath);
-
-  throw new Error(
-    [
-      `Unable to locate an Info.plist file within framework. Checked the following paths:`,
-      ...checkedPaths.map((checkedPath) => `- ${checkedPath}`),
-    ].join("\n"),
-  );
-}
-
-/**
- * Resolves the Info.plist file within a framework and reads its contents.
- */
-export async function readInfoPlist(infoPlistPath: string) {
-  try {
-    const contents = await fs.promises.readFile(infoPlistPath, "utf-8");
-    return plist.parse(contents) as Record<string, unknown>;
-  } catch (cause) {
-    throw new Error(`Unable to read Info.plist at path "${infoPlistPath}"`, {
-      cause,
-    });
-  }
-}
-
-type UpdateInfoPlistOptions = {
+type LinkFrameworkOptions = {
   frameworkPath: string;
-  oldLibraryName: string;
   newLibraryName: string;
 };
 
-/**
- * Update the Info.plist file of an xcframework to use the new library name.
- */
-export async function updateInfoPlist({
+export async function linkFramework({
   frameworkPath,
-  oldLibraryName,
   newLibraryName,
-}: UpdateInfoPlistOptions) {
-  const infoPlistPath = determineInfoPlistPath(frameworkPath);
-
-  // Convert to XML format if needed
-  try {
-    assert(
-      process.platform === "darwin",
-      "Updating Info.plist files are not supported on this platform",
-    );
-    await spawn("plutil", ["-convert", "xml1", infoPlistPath], {
-      outputMode: "inherit",
-    });
-  } catch (error) {
-    throw new Error(
-      `Failed to convert Info.plist at path "${infoPlistPath}" to XML format`,
-      { cause: error },
-    );
-  }
-
-  const contents = await readInfoPlist(infoPlistPath);
+}: LinkFrameworkOptions) {
   assert.equal(
-    contents.CFBundleExecutable,
-    oldLibraryName,
-    "Unexpected CFBundleExecutable value in Info.plist",
+    process.platform,
+    "darwin",
+    "Linking Apple frameworks are only supported on macOS",
   );
-  contents.CFBundleExecutable = newLibraryName;
-  await fs.promises.writeFile(infoPlistPath, plist.build(contents), "utf-8");
+  assert(
+    fs.existsSync(frameworkPath),
+    `Expected framework at '${frameworkPath}'`,
+  );
+  if (fs.existsSync(path.join(frameworkPath, "Versions"))) {
+    await linkVersionedFramework({ frameworkPath, newLibraryName });
+  } else {
+    await linkFlatFramework({ frameworkPath, newLibraryName });
+  }
+}
+
+export async function linkFlatFramework({
+  frameworkPath,
+  newLibraryName,
+}: LinkFrameworkOptions) {
+  assert.equal(
+    process.platform,
+    "darwin",
+    "Linking Apple addons are only supported on macOS",
+  );
+  const frameworkInfoPath = path.join(frameworkPath, "Info.plist");
+  const frameworkInfo = await readFrameworkInfo(frameworkInfoPath);
+  // Update install name
+  await spawn(
+    "install_name_tool",
+    [
+      "-id",
+      `@rpath/${newLibraryName}.framework/${newLibraryName}`,
+      frameworkInfo.CFBundleExecutable,
+    ],
+    {
+      outputMode: "buffered",
+      cwd: frameworkPath,
+    },
+  );
+  await writeFrameworkInfo(frameworkInfoPath, {
+    ...frameworkInfo,
+    CFBundleExecutable: newLibraryName,
+  });
+  // Rename the actual binary
+  await fs.promises.rename(
+    path.join(frameworkPath, frameworkInfo.CFBundleExecutable),
+    path.join(frameworkPath, newLibraryName),
+  );
+  // Rename the framework directory
+  await fs.promises.rename(
+    frameworkPath,
+    path.join(path.dirname(frameworkPath), `${newLibraryName}.framework`),
+  );
+}
+
+export async function linkVersionedFramework({
+  frameworkPath,
+  newLibraryName,
+}: LinkFrameworkOptions) {
+  assert.equal(
+    process.platform,
+    "darwin",
+    "Linking Apple addons are only supported on macOS",
+  );
+  const frameworkInfoPath = path.join(frameworkPath, "Resources", "Info.plist");
+  const frameworkInfo = await readFrameworkInfo(frameworkInfoPath);
+  // Update install name
+  await spawn(
+    "install_name_tool",
+    [
+      "-id",
+      `@rpath/${newLibraryName}.framework/${newLibraryName}`,
+      frameworkInfo.CFBundleExecutable,
+    ],
+    {
+      outputMode: "buffered",
+      cwd: frameworkPath,
+    },
+  );
+  await writeFrameworkInfo(frameworkInfoPath, {
+    ...frameworkInfo,
+    CFBundleExecutable: newLibraryName,
+  });
+  // Rename the actual binary
+  const existingBinaryPath = path.join(
+    frameworkPath,
+    frameworkInfo.CFBundleExecutable,
+  );
+  const stat = await fs.promises.lstat(existingBinaryPath);
+  assert(
+    stat.isSymbolicLink(),
+    `Expected binary to be a symlink: ${existingBinaryPath}`,
+  );
+  const realBinaryPath = await fs.promises.realpath(existingBinaryPath);
+  const newRealBinaryPath = path.join(
+    path.dirname(realBinaryPath),
+    newLibraryName,
+  );
+  // Rename the real binary file
+  await fs.promises.rename(realBinaryPath, newRealBinaryPath);
+  // Remove the old binary symlink
+  await fs.promises.unlink(existingBinaryPath);
+  // Create a new symlink with the new name
+  const newBinarySymlinkTarget = path.join(
+    "Versions",
+    "Current",
+    newLibraryName,
+  );
+  assert(
+    fs.existsSync(path.join(frameworkPath, newBinarySymlinkTarget)),
+    "Expected new binary to exist",
+  );
+  await fs.promises.symlink(
+    newBinarySymlinkTarget,
+    path.join(frameworkPath, newLibraryName),
+  );
+
+  // Rename the framework directory
+  await fs.promises.rename(
+    frameworkPath,
+    path.join(path.dirname(frameworkPath), `${newLibraryName}.framework`),
+  );
 }
 
 export async function linkXcframework({
@@ -188,10 +238,13 @@ export async function linkXcframework({
   incremental,
   naming,
 }: LinkModuleOptions): Promise<LinkModuleResult> {
+  assert.equal(
+    process.platform,
+    "darwin",
+    "Linking Apple addons are only supported on macOS",
+  );
   // Copy the xcframework to the output directory and rename the framework and binary
   const newLibraryName = getLibraryName(modulePath, naming);
-  const newFrameworkRelativePath = `${newLibraryName}.framework`;
-  const newBinaryRelativePath = `${newFrameworkRelativePath}/${newLibraryName}`;
   const outputPath = getLinkedModuleOutputPath(platform, modulePath, naming);
 
   if (incremental && fs.existsSync(outputPath)) {
@@ -209,9 +262,12 @@ export async function linkXcframework({
   // Delete any existing xcframework (or xcodebuild will try to amend it)
   await fs.promises.rm(outputPath, { recursive: true, force: true });
   // Copy the existing xcframework to the output path
-  await fs.promises.cp(modulePath, outputPath, { recursive: true });
+  await fs.promises.cp(modulePath, outputPath, {
+    recursive: true,
+    verbatimSymlinks: true,
+  });
 
-  const info = await readXcframeworkInfo(outputPath);
+  const info = await readXcframeworkInfo(path.join(outputPath, "Info.plist"));
 
   await Promise.all(
     info.AvailableLibraries.map(async (framework) => {
@@ -220,38 +276,7 @@ export async function linkXcframework({
         framework.LibraryIdentifier,
         framework.LibraryPath,
       );
-      assert(
-        fs.existsSync(frameworkPath),
-        `Expected framework at '${frameworkPath}'`,
-      );
-      const frameworkInfo = await readFrameworkInfo(frameworkPath);
-      // Update install name
-      await spawn(
-        "install_name_tool",
-        [
-          "-id",
-          `@rpath/${newBinaryRelativePath}`,
-          frameworkInfo.CFBundleExecutable,
-        ],
-        {
-          outputMode: "buffered",
-          cwd: frameworkPath,
-        },
-      );
-      await writeFrameworkInfo(frameworkPath, {
-        ...frameworkInfo,
-        CFBundleExecutable: newLibraryName,
-      });
-      // Rename the actual binary
-      await fs.promises.rename(
-        path.join(frameworkPath, frameworkInfo.CFBundleExecutable),
-        path.join(frameworkPath, newLibraryName),
-      );
-      // Rename the framework directory
-      await fs.promises.rename(
-        frameworkPath,
-        path.join(path.dirname(frameworkPath), newFrameworkRelativePath),
-      );
+      await linkFramework({ frameworkPath, newLibraryName });
     }),
   );
 
@@ -260,8 +285,8 @@ export async function linkXcframework({
     AvailableLibraries: info.AvailableLibraries.map((library) => {
       return {
         ...library,
-        BinaryPath: newBinaryRelativePath,
-        LibraryPath: newFrameworkRelativePath,
+        LibraryPath: `${newLibraryName}.framework`,
+        BinaryPath: `${newLibraryName}.framework/${newLibraryName}`,
       };
     }),
   });
