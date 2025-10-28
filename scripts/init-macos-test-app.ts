723f7b98deb95d5d7483e1beb85d2bb8793d7e49
Kræn Hansen <mail@kraenhansen.dk>
2025-10-28 14:13:03 +0100

    Patch macos app with scripts and source files

--- scripts/init-macos-test-app.ts
+++ scripts/init-macos-test-app.ts
@@ -2,11 +2,12 @@ import assert from "node:assert/strict";
 import cp from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
+import { readPackage } from "read-pkg";
 
 const REACT_NATIVE_VERSION = "0.79.6";
 const ROOT_PATH = path.join(import.meta.dirname, "..");
 const APP_PATH = path.join(ROOT_PATH, "apps", "macos-test-app");
-const HOST_PACKAGE_PATH = path.join(ROOT_PATH, "packages", "host");
+const OTHER_APP_PATH = path.join(ROOT_PATH, "apps", "test-app");
 
 function exec(command: string, args: string[], options: cp.SpawnOptions = {}) {
   const { status } = cp.spawnSync(command, args, {
@@ -50,6 +51,63 @@ async function initializeReactNativeTemplate() {
   }
 }
 
+async function patchPackageJson() {
+  console.log("Patching package.json scripts");
+  const packageJson = await readPackage({ cwd: APP_PATH });
+  const otherPackageJson = await readPackage({ cwd: OTHER_APP_PATH });
+
+  packageJson.scripts = {
+    ...packageJson.scripts,
+    metro: "react-native start --reset-cache --no-interactive",
+    "mocha-and-metro": "mocha-remote --exit-on-error -- node --run metro",
+    premacos: "killall 'MacOSTestApp' || true",
+    macos: "react-native run-macos --no-packager",
+    test: "mocha-remote --exit-on-error -- concurrently --passthrough-arguments --kill-others-on-fail npm:metro 'npm:macos -- {@}' --",
+    "test:allTests": "MOCHA_REMOTE_CONTEXT=allTests node --run test:ios -- ",
+    "test:nodeAddonExamples":
+      "MOCHA_REMOTE_CONTEXT=nodeAddonExamples node --run test -- ",
+    "test:nodeTests": "MOCHA_REMOTE_CONTEXT=nodeTests node --run test -- ",
+    "test:ferricExample":
+      "MOCHA_REMOTE_CONTEXT=ferricExample node --run test -- ",
+  };
+
+  const {
+    "mocha-remote-cli": mochaRemoteCliSpec,
+    "mocha-remote-react-native": mochaRemoteReactNativeSpec,
+  } = otherPackageJson.dependencies || {};
+
+  assert(typeof mochaRemoteCliSpec === "string");
+  assert(typeof mochaRemoteReactNativeSpec === "string");
+
+  packageJson.dependencies = {
+    "@react-native-node-api/node-addon-examples": path.relative(
+      APP_PATH,
+      path.join(ROOT_PATH, "packages", "node-addon-examples"),
+    ),
+    "@react-native-node-api/node-tests": path.relative(
+      APP_PATH,
+      path.join(ROOT_PATH, "packages", "node-tests"),
+    ),
+    "@react-native-node-api/ferric-example": path.relative(
+      APP_PATH,
+      path.join(ROOT_PATH, "packages", "ferric-example"),
+    ),
+    "react-native-node-api": path.relative(
+      APP_PATH,
+      path.join(ROOT_PATH, "packages", "host"),
+    ),
+    ["mocha-remote-cli"]: mochaRemoteCliSpec,
+    ["mocha-remote-react-native"]: mochaRemoteReactNativeSpec,
+    ...packageJson.dependencies,
+  };
+
+  await fs.promises.writeFile(
+    path.join(APP_PATH, "package.json"),
+    JSON.stringify(packageJson, null, 2),
+    "utf8",
+  );
+}
+
 function installDependencies() {
   console.log("Installing dependencies");
   exec(
@@ -60,7 +118,6 @@ function installDependencies() {
       "--prefer-offline",
       "--install-links",
       "react-native-macos-init",
-      path.relative(APP_PATH, HOST_PACKAGE_PATH),
     ],
     {
       cwd: APP_PATH,
@@ -102,8 +159,22 @@ async function patchPodfile() {
   await fs.promises.writeFile(podfilePath, podfileContents, "utf8");
 }
 
-await deletePreviousApp();
-await initializeReactNativeTemplate();
+async function copySourceFiles() {
+  console.log("Copying source files from test-app into macos-test-app:");
+  const FILE_NAMES = ["App.tsx", "babel.config.js"];
+  for (const fileName of FILE_NAMES) {
+    console.log(`↳ ${fileName}`);
+    await fs.promises.copyFile(
+      path.join(OTHER_APP_PATH, fileName),
+      path.join(APP_PATH, fileName),
+    );
+  }
+}
+
+// await deletePreviousApp();
+// await initializeReactNativeTemplate();
+await patchPackageJson();
 installDependencies();
-initializeReactNativeMacOSTemplate();
-await patchPodfile();
+// initializeReactNativeMacOSTemplate();
+// await patchPodfile();
+await copySourceFiles();
