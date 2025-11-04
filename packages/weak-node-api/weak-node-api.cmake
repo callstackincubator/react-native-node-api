723c83beaa34cd3d148f923e8980b2e020156f41
Kræn Hansen <mail@kraenhansen.dk>
2025-11-04 09:31:58 +0100

    Error if weak-node-api is included outside of cmake-rn (for now)

--- packages/weak-node-api/weak-node-api.cmake
+++ packages/weak-node-api/weak-node-api.cmake
@@ -1,3 +1,13 @@
+if(NOT DEFINED WEAK_NODE_API_LIB)
+    # TODO: Set this to an Android and Apple specific path to the dynamic library
+    message(FATAL_ERROR "WEAK_NODE_API_LIB is not set")
+endif()
+
+if(NOT DEFINED WEAK_NODE_API_INC)
+    # TODO: Set this to ./include and ./generated
+    message(FATAL_ERROR "WEAK_NODE_API_INC is not set")
+endif()
+
 add_library(weak-node-api SHARED IMPORTED)
 
 set_target_properties(weak-node-api PROPERTIES
