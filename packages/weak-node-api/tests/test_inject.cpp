9c5f8c1be7d0ba49f08a546f289cb41c6b9bb4c4
Kræn Hansen <mail@kraenhansen.dk>
2025-11-05 16:56:44 +0100

    Implemented two simple tests

--- packages/weak-node-api/tests/test_inject.cpp
+++ packages/weak-node-api/tests/test_inject.cpp
@@ -1,6 +1,25 @@
 #include <catch2/catch_test_macros.hpp>
 #include <weak_node_api.hpp>
 
-SECTION("inject_weak_node_api_host") {
-  TEST_CASE("is callable") { REQUIRE(true); }
+TEST_CASE("inject_weak_node_api_host") {
+  SECTION("is callable") {
+    WeakNodeApiHost host{};
+    inject_weak_node_api_host(host);
+  }
+
+  SECTION("propagates calls to napi_create_object") {
+    static bool called = false;
+    auto my_create_object = [](napi_env env,
+                               napi_value *result) -> napi_status {
+      called = true;
+      return napi_status::napi_ok;
+    };
+    WeakNodeApiHost host{.napi_create_object = my_create_object};
+    inject_weak_node_api_host(host);
+
+    napi_value result;
+    napi_create_object({}, &result);
+
+    REQUIRE(called);
+  }
 }
