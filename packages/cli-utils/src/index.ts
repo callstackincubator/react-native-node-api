7aaabe50b370026fab12681d230d568442633e42
Kræn Hansen <mail@kraenhansen.dk>
2025-11-14 22:48:08 +0100

    Add re-export of "p-limit" to cli-utils

--- packages/cli-utils/src/index.ts
+++ packages/cli-utils/src/index.ts
@@ -2,6 +2,7 @@ export * from "@commander-js/extra-typings";
 export { default as chalk } from "chalk";
 export * from "ora";
 export * from "bufout";
+export { default as pLimit } from "p-limit";
 
 export * from "./actions.js";
 export * from "./errors.js";
