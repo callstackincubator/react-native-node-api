124a46a950714a1de94435596b7e3fe8b01c14da
Kræn Hansen <kraen@elevenlabs.io>
2026-02-14 20:26:55 +0100

    Use where on Windows

--- packages/cmake-rn/src/ccache.ts
+++ packages/cmake-rn/src/ccache.ts
@@ -1,7 +1,10 @@
 import cp from "node:child_process";
 
 export function getCcachePath(): string | null {
-  const result = cp.spawnSync("which", ["ccache"]);
+  const result = cp.spawnSync(
+    process.platform === "win32" ? "where" : "which",
+    ["ccache"],
+  );
   if (result.status === 0) {
     return result.stdout.toString().trim();
   } else {
