c7bd14a5f572381dd22154f009b7d93ebe621900
Kræn Hansen <mail@kraenhansen.dk>
2026-02-16 13:21:31 +0100

    Use assignment instead of ||= operator now that it's gated by exit_hooks_installed

--- packages/host/scripts/patch-xcode-project.rb
+++ packages/host/scripts/patch-xcode-project.rb
@@ -2,9 +2,9 @@ unless defined?(@exit_hooks_installed)
   # Setting a flag to avoid running this command on every require
   @exit_hooks_installed = true
 
-  NODE_BINARY ||= ENV["NODE_BINARY"] || `command -v node`.strip
-  CLI_COMMAND ||= "'#{NODE_BINARY}' '#{File.join(__dir__, "../dist/node/cli/run.js")}'"
-  PATCH_XCODE_PROJECT_COMMAND ||= "#{CLI_COMMAND} patch-xcode-project '#{Pod::Config.instance.installation_root}'"
+  NODE_BINARY = ENV["NODE_BINARY"] || `command -v node`.strip
+  CLI_COMMAND = "'#{NODE_BINARY}' '#{File.join(__dir__, "../dist/node/cli/run.js")}'"
+  PATCH_XCODE_PROJECT_COMMAND = "#{CLI_COMMAND} patch-xcode-project '#{Pod::Config.instance.installation_root}'"
   
   # Using an at_exit hook to ensure the command is executed after the pod install is complete
   at_exit do
