4be7486509cf8abb9d41383c02118d54368b9eb5
Kræn Hansen <mail@kraenhansen.dk>
2026-02-16 13:18:03 +0100

    Use NODE_BINARY and read it off the environment

--- packages/host/scripts/patch-xcode-project.rb
+++ packages/host/scripts/patch-xcode-project.rb
@@ -2,8 +2,8 @@ unless defined?(@exit_hooks_installed)
   # Setting a flag to avoid running this command on every require
   @exit_hooks_installed = true
 
-  NODE_PATH ||= `which node`.strip
-  CLI_COMMAND ||= "'#{NODE_PATH}' '#{File.join(__dir__, "../dist/node/cli/run.js")}'"
+  NODE_BINARY ||= ENV["NODE_BINARY"] || `command -v node`.strip
+  CLI_COMMAND ||= "'#{NODE_BINARY}' '#{File.join(__dir__, "../dist/node/cli/run.js")}'"
   PATCH_XCODE_PROJECT_COMMAND ||= "#{CLI_COMMAND} patch-xcode-project '#{Pod::Config.instance.installation_root}'"
   
   # Using an at_exit hook to ensure the command is executed after the pod install is complete
